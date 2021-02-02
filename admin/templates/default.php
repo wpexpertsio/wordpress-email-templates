0b7b0ddee933403ccd1012925147b0fe53383e58
Damian Logghe <info@timersys.com>
2021-02-02 19:39:14 -0300

    fixed template not being included in multiple emails

--- admin/templates/default.php
+++ admin/templates/default.php
@@ -1,5 +1,5 @@
 <?php
 $settings = Mailtpl::opts();
-include_once( 'partials/header.php' );
-include_once( 'partials/email-content.php');
-include_once( 'partials/footer.php' );
+include( 'partials/header.php' );
+include( 'partials/email-content.php');
+include( 'partials/footer.php' );
